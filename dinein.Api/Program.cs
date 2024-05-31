77470bd6546a355ae6bb5874530445c8cf005864
alex-mateias <mateias.ionut.x6h@student.ucv.ro>
2024-05-31 13:43:36 +0300

    ✨ Added AuthenticationService.

--- dinein.Api/Program.cs
+++ dinein.Api/Program.cs
@@ -1,4 +1,8 @@
+using dinein.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
---
+++ dinein.Application/Services/Authentication/AuthenticationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace dinein.Application.Services.Authentication
+{
+    public record AuthenticationResult(
+        Guid Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string Token);
+}
